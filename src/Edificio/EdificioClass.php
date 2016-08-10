e707f3c4cba839b71841845142e6aaa687006cfa
Bernix01 <gbernal096@gmail.com>
2016-08-09 21:51:40 -0500

    preparando para el autoload con PSR-4

---
+++ src/Edificio/EdificioClass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace mas_aceso\Edificio;
+
+/**
+ *
+ */
+class EdificioClass
+{
+    private $nombre;
+    private $nivelAcceso;
+    private $categoria;
+    private
+
+    public function __construct()
+    {
+      //TODO hacer todo?
+    }
+}
+?>
