78ca674e0f898aed0f74b46e9eb9b00b573fadc4
Bernix01 <gbernal096@gmail.com>
2016-08-14 21:24:42 -0500

    Clase de edificio.

--- src/Edificio/EdificioClass.php
+++ src/Edificio/EdificioClass.php
@@ -2,19 +2,68 @@
 
 namespace mas_aceso\Edificio;
 
+using mas_aceso\Edificio\Reportes\ReporteCollector;
+using mas_aceso\Edificio\Categorias\CategoriaCollector;
 /**
  *
  */
 class EdificioClass
 {
-    private $nombre;
-    private $nivelAcceso;
-    private $categoria;
-    private
+    private $e_id;
+    private $e_name;
+    private $e_dscr;
+    private $e_prcnt_accs;
+    private $e_report;
+    private $e_cid;
 
     public function __construct()
     {
-      //TODO hacer todo?
+
     }
+
+    public function getId()
+    {
+      return $this->$e_id;
+    }
+
+    public function getNombre()
+    {
+      return $this->$e_name;
+    }
+
+    public function getDescripcion()
+    {
+      return $this->$e_dscr;
+    }
+
+    public function getAccesibilidad()
+    {
+      return $this->$e_prcnt_accs;
+    }
+
+    public function getReporte()
+    {
+      return ReporteCollector::getById($this->$e_report);
+    }
+
+    public function getCategoria()
+    {
+      return CategoriaCollector::getById($this->$e_cid);
+    }
+
+    public function setNombre($n)
+    {
+      $this->$e_name = $n;
+    }
+    public function setDescripcion($n)
+    {
+      $this->$e_dscr = $n;
+    }
+
+    public function setCategoria($categoria)
+    {
+      $this->$e_cid = $categoria->getId();
+    }
+    
 }
 ?>
