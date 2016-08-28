71656d0c2978c6da9ef665241970309643b7e3ec
Bernix01 <gbernal096@gmail.com>
2016-08-27 20:07:24 -0500

    agregado método genérico "getById"

--- src/util/Collector.php
+++ src/util/Collector.php
@@ -27,15 +27,42 @@ class Collector
         }
     }
 
-    public function execQuery($query)
-    {
-        try {
-            $stmt = $this->con->prepare($query);
-            $stmt->execute();
-            return true;
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            return false;
-        }
-    }
+	/**
+	 * Busca un objeto por un $id dado.
+	 * @param  int $id     El id del objeto a buscar.
+	 * @param  String $table  Nombre de la tabla en el que se buscará el objeto.
+	 * @param  String $id_col Nombre de la columna que contiene el id del objeto.
+	 * @param  string $class  Nombre de la clase en la cual se retornará el objeto.
+	 * @return mixed     una instancia de $class si se encuentra el objeto, caso contrario retorna false.
+	 */
+	public function getById($id,$table,$id_col,$class='stdClass')
+	{
+		try
+		{
+			$queryRead= 'SELECT * FROM '. $table . 'WHERE '.$id_col . '=' . $id;
+			$stmt = $this->con->prepare($queryRead);
+			$stmt->execute();
+			$result = $stmt->fetchObject($class);
+			return $result;
+		}
+		catch(PDOException $e)
+		{
+		 echo $e->getMessage();
+		 return false;
+		}
+	}
+
+	public function execQuery($query){
+		try
+		{
+			$stmt = $this->con->prepare($query);
+			$stmt->execute();
+			return true;
+		}
+		catch(PDOException $e)
+		{
+		 echo $e->getMessage();
+		 return false;
+		}
+	}
 }
