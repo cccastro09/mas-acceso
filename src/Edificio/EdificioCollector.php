1a9fd5e9d1a7871c5082ff2b350acefd740bb091
Bernix01 <gbernal096@gmail.com>
2016-08-27 20:07:35 -0500

    inicio de EdificioCollector

---
+++ src/Edificio/EdificioCollector.php
@@ -0,0 +1,39 @@
+<?php
+namespace mas_acceso\Edificio;
+
+use mas_acceso\Util\Collector;
+use mas_acceso\Edificio\EdificioClass;
+
+/**
+ * Colector de Edificio
+ */
+class EdificioCollector extends Collector
+{
+
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   * Obtiene todos los edificios agregar paginación es un TODO
+   * @return mixed array() de EdificioClass o false si hubo algún error.
+   */
+  public function getAllEdificios()
+  {
+    parent::read('edificio','Edificio');
+  }
+
+  /**
+   * Obtiene un edificio en específico
+   * @param  string $id El id del edificio.
+   * @return mixed      El Edificio si se encuentra, caso contrario retorna false.
+   */
+  public function getEdificio($id)
+  {
+    parent::getById($id,'edificio','e_id','Edificio');
+  }
+
+
+
+}
