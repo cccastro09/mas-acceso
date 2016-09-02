dfa6859a4911b3c477dbb356fa7c3da70cc8b515
kevinmalats <ken_1721@hotmail.es>
2016-09-02 14:17:23 -0500

    corriguiendo

--- src/autoload.php
+++ src/autoload.php
@@ -9,9 +9,9 @@ spl_autoload_register(
               'mas_aceso\\edificio\\edificio' => '/src/Edificio/EdificioClass.php',
               'mas_aceso\\util\\database\\database' => '/src/util/database/Database.php',
                  'mas_aceso\\util'  => '/src/util/Collector.php',
-                'Rol' => 'src/Usuario/Rol/ColectorRol.php',
-                 'Rol' => 'src/Usuario/Rol/RolClass.php',
-                'Rol' => 'src/Usuario/Rol/index.php'
+                'mas_acceso\usuario\Rol' => 'src/Usuario/Rol/ColectorRol.php',
+                 'mas_acceso\usuario\Rol' => 'src/Usuario/Rol/RolClass.php',
+                'mas_acceso\usuario\Rol' => 'src/Usuario/Rol/index.php'
                 
                 
             ];
