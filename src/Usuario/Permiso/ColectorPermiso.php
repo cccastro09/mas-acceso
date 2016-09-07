2646e7814464f658d3d97bf151abb96af34009eb
kevinmalats <ken_1721@hotmail.es>
2016-09-07 10:06:38 -0500

    creando Permiso

---
+++ src/Usuario/Permiso/ColectorPermiso.php
@@ -0,0 +1,65 @@
+<?php
+namespace mas_acceso\usuario\Permiso;
+
+//require_once "RolClass.php";
+//require_once "../../../autoload.php";
+use mas_acceso\util\Collector;
+
+class ColectorPermiso extends Collector
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function addPermiso($per)
+    {
+        return self::execQuery("INSERT INTO permiso(p_constante, p_descripcion) VALUES('".$per->getConstante()."','".$per->getDescripcion()."')");
+    }
+
+  /* public function getNombre($id)
+   {
+
+    $stmt = $this->con->prepare("SELECT * FROM demo WHERE id=:id");
+    $stmt->execute(array(":id"=>$id));
+    $usuario=$stmt->fetchObject("Demo");
+    return $usuario;
+   }
+   public function readAlldemo(){
+
+      return self::read('demo','demo');
+
+
+  }*/
+    public function leerPermiso()
+    {
+
+        return self::read('permiso', 'PermisoClass');
+    }
+    public function updateRol($per)
+    {
+        try {
+             self::execQuery("UPDATE rol SET p_id='".$per->getId()."',p_descripcion='".$per->getPermiso()."',p_constante='".$per->getConstante()."' WHERE p_id=".$per->getId());
+
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+    public function getRol($id){
+         return self::getBYId($id, 'permiso', 'p_id', 'PermisoClass');
+    }
+
+    public function deleteRol($perl)
+    {
+        try {
+             self::execQuery("DELETE FROM rol WHERE p_id=".$per);
+
+            return true;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
+    }
+}
