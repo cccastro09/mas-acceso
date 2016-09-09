9e4461636ba44d51519fec1317775750c3670537
Bernix01 <gbernal096@gmail.com>
2016-09-08 19:35:34 -0500

    comentario actualizado

--- partes/comentario.php
+++ partes/comentario.php
@@ -3,25 +3,25 @@
       /*require_once $_SERVER['DOCUMENT_ROOT'].'/src/autoload.php';*/
       use mas_acceso\edificio\comentario\ComentarioCollector;
       use mas_acceso\edificio\comentario\ComentarioClass;
-      $colector= new ComentarioCollector();
-      if(isset($_POST["coment"])){
-
-        $comentario = new ComentarioClass();
-        $comentario->setContenido($_POST["coment"]);
-        $comentario->setIdEdificio($_GET["e_id"]); //llamar a edificio de guillermo
-        $comentario->setIdUsuario(3); //llamar a usuario de Claudia
 
       $colector= new ComentarioCollector();
 if (isset($_POST["coment"])) {
     $comentario = new ComentarioClass();
     $comentario->setContenido($_POST["coment"]);
-    $comentario->setIdEdificio(2); //llamar a edificio de guillermo
+    $comentario->setIdEdificio($_GET["e_id"]); //llamar a edificio de guillermo
     $comentario->setIdUsuario(3); //llamar a usuario de Claudia
 
-      foreach ($colector->readAllComentarioByEdificio($_GET["e_id"]) as $datos) {
-?>
+    if ($colector->addComentario($comentario)) {
+        // refrescar a la pagina actual
+        /*header("Location: /src/Edificio");*/
+    } else {
+        echo "Hubo un error al intentar agregar el comentario2.";
+    }
+} else {
+    foreach (($colector->readAllComentarioByEdificio($_GET["e_id"])) as $datos) {
+    ?>
 
-<div class="col-xs-12">
+    <div class="col-xs-12">
     <?php /*echo usuario->getNombre()*/ echo $datos->getContenido(); ?>
 </div>
 
